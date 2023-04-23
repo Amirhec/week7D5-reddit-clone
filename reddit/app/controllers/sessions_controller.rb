c317c43cbe7a283d8ab3f2441762091af84659c8
Amir Lamrabat <secretsofthecode9@gmail.com>
2023-04-23 14:49:06 -0400

    phase 1

--- reddit/app/controllers/sessions_controller.rb
+++ reddit/app/controllers/sessions_controller.rb
@@ -1,2 +1,32 @@
 class SessionsController < ApplicationController
+    before_action :require_logged_in, only:[:destroy]
+    before_action :require_logged_out, only: [:new,:create]
+
+    def new 
+        @user = User.new
+        render :new
+    end
+
+    def create 
+        username = params[:user][:username]
+        password = params[:user][:password]
+
+        @user = User.find_by_credentials(username,password)
+
+        if @user
+            login!(@user)
+            redirect_to users_url 
+        else
+            @user = User.new(username: username)
+            flash.now[:errors] = ["invalid username or password"]
+            render :new 
+        end
+
+
+    end
+
+    def logout! 
+        logout!
+        redirect_to new_session_url
+    end
 end
--- reddit/config/routes.rb
+++ reddit/config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :users
+  resources :subs 
+  resources :posts, except:[:destroy]
+  resource :session, only:[:new,:create,:destroy]
+
 end
