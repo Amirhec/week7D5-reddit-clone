6da23b8a4b8c36b2e4b18b8991b99456f64ac752
Amir Lamrabat <secretsofthecode9@gmail.com>
2023-04-23 13:52:02 -0400

    migration

--- reddit/db/migrate/20230423173952_create_subs.rb
+++ reddit/db/migrate/20230423173952_create_subs.rb
@@ -2,8 +2,8 @@ class CreateSubs < ActiveRecord::Migration[7.0]
   def change
     create_table :subs do |t|
       t.string :title
-      t.string :description
-      t.bigint :user_id
+      t.string :description,null:false 
+      t.bigint :user_id, null:false 
 
       t.timestamps
     end
--- reddit/db/migrate/20230423174415_create_posts.rb
+++ reddit/db/migrate/20230423174415_create_posts.rb
@@ -1,11 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.string :title
+      t.string :title,null: false 
       t.string :url
       t.string :content
-      t.bigint :sub_id
-      t.bigint :author_id
+      t.bigint :sub_id, null:false 
+      t.bigint :author_id, null:false 
 
       t.timestamps
     end
